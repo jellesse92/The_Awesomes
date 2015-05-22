f30a2847de27595ed6fdca9e7fe548ff50697da1
jellesse92 <jehunter@uci.edu>
2015-05-22 13:06:28 -0700

    Scripting for Pickup items

---
+++ ICS62 Project/Assets/Scenes/PickUP/PickUp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+
+	//Game Logic
+	GameController gc;
+	// Use this for initialization
+	void Start () {
+		gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider obj){
+		if (obj.tag == "Break") {
+			gc.playerVar = gc.breakCondition;
+			gameObject.SetActive (false);
+		} 
+		if (obj.tag == "PlusEQ") {
+			gc.playerVar += 1;
+			gameObject.SetActive (false);
+		}
+		if (obj.tag == "MinusEQ") {
+			gc.playerVar -=1;
+			gameObject.SetActive (false);
+		}
+	}
+
+}
