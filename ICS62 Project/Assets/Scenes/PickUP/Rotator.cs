3f9250961bd77f481fb3383c169184c2c22c8815
jellesse92 <jehunter@uci.edu>
2015-05-22 11:56:01 -0700

    Texturing and Scripting for Pickup ITems

---
+++ ICS62 Project/Assets/Scenes/PickUP/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public int speed = 10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, Time.deltaTime * speed, 0));
+	}
+}
