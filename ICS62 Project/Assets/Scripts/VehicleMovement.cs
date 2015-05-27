1f44ffed81421b8240044188745dbbb5cbc548a9
rwbysafire <tomtan117@gmail.com>
2015-05-27 12:48:36 -0700

    VehicleMovement Updated again

--- ICS62 Project/Assets/Scripts/VehicleMovement.cs
+++ ICS62 Project/Assets/Scripts/VehicleMovement.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class VehicleMovement : MonoBehaviour {
@@ -6,7 +6,8 @@ public class VehicleMovement : MonoBehaviour {
 	public float speed;
 	public float strafe; 
 	public float turnSpeed;
-	//public float groundDist;
+	public float hoverForce;
+	public float hoverDist;
 	private Rigidbody rb;
 	private float groundDist;
 	
@@ -20,9 +21,12 @@ public class VehicleMovement : MonoBehaviour {
 		RaycastHit rcHit;
 		float newPosition = transform.position.y; 
 		if (Physics.Raycast(transform.position, Vector3.down, out rcHit)){
-			groundDist = rcHit.distance;
+			float proportionalHeight = (hoverDist - rcHit.distance)/hoverDist;
+			Vector3 appliedForce = Vector2.up * proportionalHeight * hoverForce;
+			rb.AddForce(appliedForce, ForceMode.Acceleration);
+			//groundDist = rcHit.distance;
 			//transform.rotation = Quaternion.FromToRotation(Vector3.up, rcHit.normal);
-			newPosition = (newPosition - groundDist) + 1;
+			//newPosition = (newPosition - groundDist) + 1;
 
 		}
 		transform.position = new Vector3(transform.position.x, newPosition, transform.position.z);
