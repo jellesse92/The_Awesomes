095a93a4f15ba41a60e3b4e0324c436dfcbb6a0e
rwbysafire <tomtan117@gmail.com>
2015-05-29 17:06:13 -0700

    Revert "Revert "more update""
    This reverts commit a0a09301954b6d1964d2532c20f4a21fbb5d53fb.

--- ICS62 Project/Assets/Scripts/Teleport.cs
+++ ICS62 Project/Assets/Scripts/Teleport.cs
@@ -3,13 +3,10 @@ using System.Collections;
 
 public class Teleport : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player"){
+			other.transform.position = GameObject.Find("Portal").transform.position;
+		}
 	}
 }
